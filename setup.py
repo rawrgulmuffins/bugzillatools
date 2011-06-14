23ee9eed58218919120cf546ac89b293c73caffd
Fraser Tweedale <frase@frase.id.au>
2011-06-14 21:58:40 +1000

    update README and bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README') as fh:
 
 setup(
     name='bugzillatools',
-    version='0.1',
+    version='0.1.1',
     description='Bugzilla CLI client and XML-RPC interface library',
     author='Fraser Tweedale',
     author_email='frasert@jumbolotteries.com',
