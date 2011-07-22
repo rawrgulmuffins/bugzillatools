4dcbd299e4c76b17268e605a994837455a0f19b4
Fraser Tweedale <frasert@jumbolotteries.com>
2011-07-22 16:17:16 +1000

    bzr - write help for bugzillatools plugin

--- plugin-bzr/__init__.py
+++ plugin-bzr/__init__.py
@@ -14,12 +14,76 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-"""
-Mark bugzilla bugs fixed using --fixes argument
+"""Mark bugzilla bugs fixed using --fixes argument.
 
-If the --fixes argument to `bzr commit` points to a bugzilla installation,
-the bug will be marked fixed using the credentials stored in your
-bazaar.conf.
+Description
+-----------
+
+This plugin, when enabled for Bugzilla bugtrackers, marks bugs fixed on
+those trackers when ``bzr commit`` is invoked with the ``fixes`` argument.
+
+Configuration
+-------------
+
+Define the bugtracker in your ``bazaar.conf`` in the normal fashion, and
+enable *bugzillatools* for that tracker.
+
+    bugzilla_<handle>_url = <url>
+    bugzilla_<handle>_bugzillatools = True
+
+If the handle matches a server in your ``.bugzillarc``, no further
+configuration is needed; the parameters from this file will be used.  The
+URLs should not differ between the two configuration files, but if they do,
+``.bugzillarc`` takes precendence.
+
+If the handle does not match a server in your ``.bugzillarc``, the Bugzilla
+URL, user and password are determined thusly:
+
+- For the url, the value of ``bugzilla_<handle>_url``.
+- For the username, ``bugzilla_<handle>_user`` or if this is not defined, the
+  email address of the committer.
+- For the password, ``bugzilla_<handle>_password``, which must be defined.
+
+The status and resolution of "fixed" bugs must also be specified for each
+tracker, since these are configurable and may differ between installations
+of Bugzilla.  For example, in your ``bazaar.conf``::
+
+    bugzilla_<handle>_status = RESOLVED
+    bugzilla_<handle>_resolution = FIXED
+
+Example
+-------
+
+A Bugzilla server at ``http://bugzilla.example.com``, with user
+``bob@example.com`` and password ``bob123``.
+
+``.bugzillarc``::
+
+    ...
+    "servers": {
+        "example": ["http://bugzilla.example.com", "bob@example.com", "bob123"]
+    }
+    ...
+
+``bazaar.conf``::
+
+    bugzilla_example_url = http://bugzilla.example.com/
+    bugzilla_example_bugzillatools_enable = True
+    bugzilla_example_status = RESOLVED
+    bugzilla_example_resolution = FIXED
+
+If not defining ``example`` in ``.bugzillarc``, also include::
+
+    bugzilla_example_password = bob123
+
+and, if Bob is not committing as ``bob@example.com``, also include::
+
+    bugzilla_example_user = bob@example.com
+
+To mark a bug fixed (in addition to the standard behaviour of recording
+a bug URL and status in the revision metadata), invoke Bazaar thusly::
+
+    bzr commit -m 'fix bug 123' --fixes example:123
 """
 
 import bzrlib.api
