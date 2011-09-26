61bc0d9b003aa43eb7abbc98b9bc026e41b23a44
Fraser Tweedale <frase@frase.id.au>
2011-09-26 19:35:59 +1000

    bzlib.bugzilla (& tests) - update copyright and license

--- bzlib/bugzilla.py
+++ bzlib/bugzilla.py
@@ -1,5 +1,5 @@
 # This file is part of bugzillatools
-# Copyright (C) 2011 Benon Technologies Pty Ltd
+# Copyright (C) 2011 Benon Technologies Pty Ltd, Fraser Tweedale
 #
 # bugzillatools is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
