542c648427812ec915565e7ad6831f6d30be1a99
Fraser Tweedale <frase@frase.id.au>
2013-11-23 11:55:00 +1000

    ui: fix unicode encoding error

--- bzlib/ui.py
+++ bzlib/ui.py
@@ -1,5 +1,6 @@
 # This file is part of bugzillatools
-# Copyright (C) 2011, 2012 Fraser Tweedale, Benon Technologies Pty Ltd
+# Copyright (C) 2011, 2012, 2013  Fraser Tweedale
+# Copyright (C) 2011, 2012  Benon Technologies Pty Ltd
 #
 # bugzillatools is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -13,6 +14,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals
 
 import functools
 import math
